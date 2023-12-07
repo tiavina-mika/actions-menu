c42cd93982ead89c29217e41b994e802fa545cb8
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-12-07 17:32:49 +0300

    change title

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -28,9 +28,7 @@ const Home = () => {
         css={classes.content}
         className="stretchSelf flexRow center spaceBetween"
       >
-        <Typography variant="h3">
-          A project starter for Material UI 5, Emotion and TypeScript
-        </Typography>
+        <Typography variant="h3">Click the menus right</Typography>
         <ActionsModal title="What we do?" menus={menus} />
       </div>
       <Footer />
