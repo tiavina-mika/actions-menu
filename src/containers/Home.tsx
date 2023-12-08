9b89fce6b69321216b06d5e4c2fc1e9444283cf2
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-12-08 10:19:03 +0300

    add content

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -5,6 +5,7 @@ import { Typography } from "@mui/material";
 import { LAYOUT_CONTENT_PADDING } from "../utils/constants";
 import Footer from "../components/Footer";
 import ActionsModal from "./ActionsModal";
+import { Theme } from "@emotion/react";
 
 const classes = {
   root: {
@@ -12,7 +13,11 @@ const classes = {
   },
   content: {
     padding: LAYOUT_CONTENT_PADDING
-  }
+  },
+  header: (theme: Theme) => ({
+    borderBottom: "1px solid " + theme.palette.grey[300],
+    padding: `12px ${LAYOUT_CONTENT_PADDING}px`
+  })
 };
 
 const Home = () => {
@@ -24,12 +29,16 @@ const Home = () => {
   ];
   return (
     <div className="flexColumn spaceBetween" css={classes.root}>
-      <div
-        css={classes.content}
-        className="stretchSelf flexRow center spaceBetween"
-      >
-        <Typography variant="h3">Hello</Typography>
-        <ActionsModal title="What we do?" menus={menus} />
+      <div className="stretchSelf">
+        <div css={classes.header} className="flexRow center spaceBetween">
+          <Typography variant="h3">Mik.</Typography>
+          <ActionsModal title="What we do?" menus={menus} />
+        </div>
+        <div css={classes.content}>
+          <Typography className="grey800">
+            Click the icon in the top right corner to open the menu
+          </Typography>
+        </div>
       </div>
       <Footer />
     </div>
